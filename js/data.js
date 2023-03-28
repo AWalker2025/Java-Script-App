84a953abb39ea9c7e6754dfed94550cd8522d76d
AWalker2025 <112598458+AWalker2025@users.noreply.github.com>
2023-03-28 14:12:42 -0400

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,172 @@
+const data = [
+
+     {    
+            "title": "Movie Night",  
+            "content": "Have a movie night at home with your favorite films and snacks",  
+             "tag": "Stay In",    
+            "photo": "https://i0.wp.com/onechelofanadventure.com/wp-content/uploads/2021/01/At-home-movie-screen-projector.png?fit=750%2C500&ssl=1"  
+        },  
+
+        {    
+            "title": "DIY Wine and Cheese Night",  
+            "content": "Create your own wine and cheese tasting at home. Pick out a few different types of cheese and wine and enjoy pairing them together.", 
+             "tag": "Stay In",    
+            "photo": "https://static.pariswinecup.com/en/blog/images/w/1200/ParisWineCup-07152019102010000000-5d2c535a55f24.jpg"  
+        },  
+
+        {    
+            "title": "Cook Dinner Together",   
+            "content": "Choose a recipe that you both want to try and spend the evening cooking together.", 
+             "tag": "Stay In",    
+            "photo": "https://static.wixstatic.com/media/3106e5_7b8c4c21aa784593baa1c992fce77093~mv2.jpg/v1/fill/w_640,h_426,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/3106e5_7b8c4c21aa784593baa1c992fce77093~mv2.jpg"  
+        },  
+
+        {    
+            "title": "Game Night ",   
+            "content": "Choose your favorite board games, card games, or video games and spend the night competing against each other.", 
+             "tag": "Stay In",    
+            "photo": "https://kimandkalee.com/wp-content/uploads/2022/02/couple-playing-jenga-at-home.jpg"  
+        },  
+
+        {    
+            "title": "Scavenger Hunt",   
+            "content": "Create a scavenger hunt for each other, using clues that lead to different parts of the house. Hide small prizes or treats at the end of the scavenger hunt for a fun surprise.", 
+             "tag": "Stay In",    
+            "photo": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/cc/2c/c9/guided-for-the-smartphone.jpg?w=500&h=400&s=1"  
+        },  
+
+        {    
+            "title": "DIY Home Improvement Project",   
+            "content": "Pick a project you've been wanting to tackle around the house and work on it together. It could be something as simple as rearranging a room or something more complex like painting a mural. Enjoy the satisfaction of working together and seeing the finished project.", 
+             "tag": "Stay In",    
+            "photo": "https://s28126.pcdn.co/blogs/ask-experian/wp-content/uploads/Top-Personal-Loan-Lenders-for-Home-Improvement-1400x9331.jpg"  
+        },  
+
+                {
+                "title": "Dinner at a Fancy Restaurant",
+                "content": "Try out a new or highly recommended restaurant that you haven't been to before. Dress up and enjoy a nice meal together.",    
+                "tag": "Costly",    
+                "photo": "https://media.istockphoto.com/id/871870942/photo/couple-on-a-date.jpg?s=612x612&w=0&k=20&c=9sjXQrl_spcB8e6BB7BsJc3knWleq0WnM9j_DQbOOB8="  
+               },
+
+               {
+                "title": "Couples Massage",   
+                "content": "Book a couples massage at a local spa or wellness center. Relax and unwind together while being pampered.", 
+                "tag": "Costly",    
+                "photo": "https://cdn.spafinder.com/2019/09/decision-image-2-1024x683.png"  
+               },
+
+               {
+                "title": "Concert or Theater Tickets",   
+                "content": "Check out upcoming shows or concerts in your area and purchase tickets for an entertaining night out.",
+                "tag": "Costly",    
+                "photo": "https://media.istockphoto.com/id/1248394948/photo/couple-toasting-on-a-music-festival.jpg?s=612x612&w=0&k=20&c=_wDpjYxeMP79maTSsBUSPwG9qSJXvffNFTvOKxfEJyk="  
+               },
+
+               {
+                "title": "Wine Tasting",   
+                "content": "Plan a trip to a local winery or vineyard for a wine tasting experience. Learn about different types of wine and enjoy the beautiful scenery.",
+                "tag": "Costly",    
+                "photo": "https://assets.simpleviewinc.com/simpleview/image/fetch/c_fill,h_683,q_75,w_1024/https://assets.simpleviewinc.com/simpleview/image/upload/v1/clients/travelpasoca/Cheers_with_View_optimized_1024x683_3fa4d40c-6522-4a21-9b9d-cd395317e787.jpg"  
+               },
+
+               {
+                "title": "Escape Room",   
+                "content": "Test your problem-solving skills and bond with your partner by tackling an escape room challenge.",
+                "tag": "Costly",    
+                "photo": "https://escaperoomreno.net/wp-content/uploads/2020/03/playing-reno-escape-game.jpg"  
+               },
+
+               {
+                "title": "Amusement Park",   
+                "content": "Spend the day at an amusement park, riding roller coasters and playing carnival games. Enjoy the thrill of the rides and win prizes for each other.",
+                "tag": "Costly",    
+                "photo": "https://media.istockphoto.com/id/639589362/photo/couple-in-bumper-cars.jpg?s=612x612&w=0&k=20&c=7kVrCsOPtwu6r4OFfMFPitBfkDDiB1_CY_zyOzduQDA="  
+               },
+
+               {
+                "title": "Dinner and a Show",
+                "content": "Go out for a nice dinner at a restaurant and then catch a show at a local theater or comedy club.",    
+                "tag": "Go Out",    
+                "photo": "https://publish.purewow.net/wp-content/uploads/sites/2/2017/09/best-movie-couples-mr-mrs-smith.jpg?fit=728%2C524"  
+               },
+
+               {
+                "title": "Live Music",   
+                "content": "Check out a live music performance at a local venue. You can even research up-and-coming artists or local bands for a unique experience.", 
+                "tag": "Go Out",    
+                "photo": "https://cdn2.lamag.com/wp-content/uploads/sites/6/2021/09/live-music-daniel-dvorsky-DMaOGYRS4_U-unsplash-1-1068x712.jpg"  
+               },
+
+               {
+                "title": "Art Exhibit or Museum",   
+                "content": "Visit an art exhibit or museum and appreciate the beauty and creativity of various artists and exhibits.",
+                "tag": "Go Out",    
+                "photo": "https://media.istockphoto.com/id/1298329263/photo/young-couple-in-modern-art-exhibition-gallery-hall.jpg?s=612x612&w=0&k=20&c=qiuw8OlQpLIo8cIvZGuFJLl1YM9gbnIGLP-HTtGDEXU="  
+               },
+
+               {
+                "title": "Go-Karting or Mini Golf",   
+                "content": "Challenge each other to a fun game of go-karting or mini-golf. It's a great opportunity to be playful and competitive.",
+                "tag": "Go Out",    
+                "photo": "https://www.surprisedatechallenge.com/wp-content/uploads/2021/01/k1-speed-date.jpg"  
+               },
+
+               {
+                "title": "Outdoor Adventure",   
+                "content": "Plan a hiking trip or go on a bike ride together. You can explore nature and enjoy some exercise while spending quality time together.",
+                "tag": "Go Out",    
+                "photo": "https://media.istockphoto.com/id/1368171893/photo/full-length-shot-of-an-affectionate-young-couple-taking-photos-while-out-on-an-early-morning.jpg?b=1&s=170667a&w=0&k=20&c=iqUagH1Ba8gK4H-HsM_9vm_1f5jSXIAoOvrbwVv30Gk="  
+               },
+
+               {
+                "title": "Improv or Stand-up Comedy Show",   
+                "content": "Check out a live improv or stand-up comedy show at a local venue. You can enjoy some laughs and even participate in the show, depending on the type of performance. It's a great way to experience live entertainment and have a memorable night out.",
+                "tag": "Go Out",    
+                "photo": "https://img-vimbly-com-images.imgix.net/full_photos/0_new-comedy-shows-1.jpg?auto=compress&fit=crop&h=490&ixlib=php-1.2.1&w=730"  
+               },
+
+
+               {
+                "title": "Go to the Beach together",
+                "content": "Go out for a nice walk on the beach or watch the sunset together.",    
+                "tag": "Cost-less",    
+                "photo": "https://assets.churchofjesuschrist.org/68/e6/68e6e6b783786a8c595e517f105507cbcf3dc9ce/68e6e6b783786a8c595e517f105507cbcf3dc9ce.jpeg"  
+               },
+
+               {
+                "title": "Plan a Future Trip",   
+                "content": "Spend the evening planning a future trip together, researching destinations, activities, and accommodations. Dreaming and planning for the future can be a fun and bonding experience.", 
+                "tag": "Cost-less",    
+                "photo": "https://northernvirginiamag.com/wp-content/uploads/2020/06/couple-planning-travel-photo-by-adobe-stock-bullrun.jpg"  
+               },
+
+               {
+                "title": "DIY Spa Night",   
+                "content": " Have a relaxing and rejuvenating spa night at home with your partner. You can give each other massages, do face masks or take a bubble bath together. It's a great way to unwind and de-stress together.",
+                "tag": "Cost-less",    
+                "photo": "https://kimandkalee.com/wp-content/uploads/2022/04/romantic-spa-date-night.jpg"  
+               },
+
+               {
+                "title": "Volunteer Together",   
+                "content": "Find a local charity or organization and volunteer together. It's a great way to give back to the community and spend time together while making a positive impact.",
+                "tag": "Cost-less",    
+                "photo": "https://www.gozaround.com/blog/wp-content/uploads/2016/02/Volunteer-Couple.jpg"  
+               },
+
+               {
+                "title": "Stargazing",   
+                "content": "Find a quiet spot away from city lights and enjoy a beautiful night sky with your partner. You can also look up constellations together and make a wish on a shooting star.",
+                "tag": "Cost-less",    
+                "photo": "https://www.onetravel.com/going-places/wp-content/uploads/2017/07/Untitled-design-68-810x486.jpg"  
+               },
+
+               {
+                "title": "Picnic in the Park",   
+                "content": "ack a picnic basket with your favorite snacks and drinks, find a cozy spot in a nearby park and enjoy a romantic picnic together.",
+                "tag": "Cost-less",    
+                "photo": "https://media.istockphoto.com/id/913055904/photo/next-to-you-is-the-only-place-for-me.jpg?s=612x612&w=0&k=20&c=3FJWydZWGOgrJapmv5kR--QEhqZMoKI9nmno2JkqcYw="  
+               },
+            ]
+
